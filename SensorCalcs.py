3702e7e11efedf4df177c00a2270f78d3254740b
jfgist <James.Gist@gmail.com>
2014-02-05 20:05:21 -0700

    Refactoring

---
+++ SensorCalcs.py
@@ -0,0 +1,27 @@
+def floatfromhex(h):
+    t = float.fromhex(h)
+    if t > float.fromhex('7FFF'):
+        t = -(float.fromhex('FFFF') - t)
+        pass
+    return t
+
+def calcTmpTarget(objT, ambT):
+    m_tmpAmb = ambT/128.0
+    Vobj2 = objT * 0.00000015625
+    Tdie2 = m_tmpAmb + 273.15
+    S0 = 6.4E-14
+    a1 = 1.75E-3
+    a2 = -1.678E-5
+    b0 = -2.94E-5
+    b1 = -5.7E-7
+    b2 = 4.63E-9
+    c2 = 13.4
+    Tref = 298.15
+    S = S0*(1+a1*(Tdie2 - Tref)+a2*pow((Tdie2 - Tref),2))
+    Vos = b0 + b1*(Tdie2 - Tref) + b2*pow((Tdie2 - Tref),2)
+    fObj = (Vobj2 - Vos) + c2*pow((Vobj2 - Vos),2)
+    tObj = pow(pow(Tdie2,4) + (fObj/S),.25)
+    tObj = (tObj - 273.15)
+   
+    print(round(tObj,2))
+    return tObj
--- bletemp.py
+++ bletemp.py
@@ -4,6 +4,8 @@ import pexpect
 import sys
 import time
 import datetime
+from SensorTag import *
+from SensorCalcs import *
 
 csvfile = r'testfile.csv'
 #set this empty to turn off logging:
@@ -16,33 +18,33 @@ def usage():
     print '  address    The address of the sensor to read.'
     print '  interval   The reading interval in seconds.  Default is 1.'
 
-def floatfromhex(h):
-    t = float.fromhex(h)
-    if t > float.fromhex('7FFF'):
-        t = -(float.fromhex('FFFF') - t)
-        pass
-    return t
+#def floatfromhex(h):
+#    t = float.fromhex(h)
+#    if t > float.fromhex('7FFF'):
+#        t = -(float.fromhex('FFFF') - t)
+#        pass
+#    return t
 
-def calcTmpTarget(objT, ambT):
-    m_tmpAmb = ambT/128.0
-    Vobj2 = objT * 0.00000015625
-    Tdie2 = m_tmpAmb + 273.15
-    S0 = 6.4E-14
-    a1 = 1.75E-3
-    a2 = -1.678E-5
-    b0 = -2.94E-5
-    b1 = -5.7E-7
-    b2 = 4.63E-9
-    c2 = 13.4
-    Tref = 298.15
-    S = S0*(1+a1*(Tdie2 - Tref)+a2*pow((Tdie2 - Tref),2))
-    Vos = b0 + b1*(Tdie2 - Tref) + b2*pow((Tdie2 - Tref),2)
-    fObj = (Vobj2 - Vos) + c2*pow((Vobj2 - Vos),2)
-    tObj = pow(pow(Tdie2,4) + (fObj/S),.25)
-    tObj = (tObj - 273.15)
+#def calcTmpTarget(objT, ambT):
+#    m_tmpAmb = ambT/128.0
+#    Vobj2 = objT * 0.00000015625
+#    Tdie2 = m_tmpAmb + 273.15
+#    S0 = 6.4E-14
+#    a1 = 1.75E-3
+#    a2 = -1.678E-5
+#    b0 = -2.94E-5
+#    b1 = -5.7E-7
+#    b2 = 4.63E-9
+#    c2 = 13.4
+#    Tref = 298.15
+#    S = S0*(1+a1*(Tdie2 - Tref)+a2*pow((Tdie2 - Tref),2))
+#    Vos = b0 + b1*(Tdie2 - Tref) + b2*pow((Tdie2 - Tref),2)
+#    fObj = (Vobj2 - Vos) + c2*pow((Vobj2 - Vos),2)
+#    tObj = pow(pow(Tdie2,4) + (fObj/S),.25)
+#    tObj = (tObj - 273.15)
     
-    print(round(tObj,2))
-    return tObj
+#    print(round(tObj,2))
+#    return tObj
     
 def saveData(hexStr):
     #print "after expect"
@@ -68,10 +70,10 @@ def connect(tool):
     tool.sendline('char-write-cmd 0x29 01')
     tool.expect('\[LE\]>')
 
-class SensorTag:
-    def __init__(self,mac,control):
-        self.mac = mac
-        self.control = control
+#class SensorTag:
+#    def __init__(self,mac,control):
+#        self.mac = mac
+#        self.control = control
 
 def bleTempCollection(addresses, interval=1):
 
@@ -121,36 +123,6 @@ def bleTempCollection(addresses, interval=1):
                 
     # will this crash if lf is not created ?
     lf.close()        
-
-def bleTemp(bluetooth_adr, interval=1):
-
-    if logfile != '': 
-        lf = open(logfile, 'a')
-        tool = pexpect.spawn('gatttool -b ' + bluetooth_adr + ' --interactive',logfile=lf)
-    else:
-        tool = pexpect.spawn('gatttool -b ' + bluetooth_adr + ' --interactive')
-    
-    tool.expect('\[LE\]>')
-    print "Preparing to connect. You might need to press the side button"
-    #print "send connect"
-    connect(tool)
-    
-    while True:
-        time.sleep(float(interval))
-        print "poll for temperature"
-        tool.sendline('char-read-hnd 0x25')
-        print "expect descriptor"
-
-        index = tool.expect (['descriptor: .*', 'Disconnected', pexpect.EOF, pexpect.TIMEOUT],3)
-        if index == 0:
-            saveData(tool.after)
-        elif index == 1:
-            connect(tool)
-        #elif index == 2 or index == 3:
-            #print 'pexpect died, eof or timeout'
-            #exit()
-
-    lf.close()    
     
 #TODO: cleanup usage and catch erroneous input in main
     
