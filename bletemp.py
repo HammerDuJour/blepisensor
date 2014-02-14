cbe59be7434f02727b54744cd00be7e399414f80
jfgist <James.Gist@gmail.com>
2014-02-14 16:25:48 -0700

    Removing Dead Code
    Deleting some code that has since been moved to another function

--- bletemp.py
+++ bletemp.py
@@ -18,34 +18,7 @@ def usage():
     print '  address    The address of the sensor to read.'
     print '  interval   The reading interval in seconds.  Default is 1.'
 
-#def floatfromhex(h):
-#    t = float.fromhex(h)
-#    if t > float.fromhex('7FFF'):
-#        t = -(float.fromhex('FFFF') - t)
-#        pass
-#    return t
-
-#def calcTmpTarget(objT, ambT):
-#    m_tmpAmb = ambT/128.0
-#    Vobj2 = objT * 0.00000015625
-#    Tdie2 = m_tmpAmb + 273.15
-#    S0 = 6.4E-14
-#    a1 = 1.75E-3
-#    a2 = -1.678E-5
-#    b0 = -2.94E-5
-#    b1 = -5.7E-7
-#    b2 = 4.63E-9
-#    c2 = 13.4
-#    Tref = 298.15
-#    S = S0*(1+a1*(Tdie2 - Tref)+a2*pow((Tdie2 - Tref),2))
-#    Vos = b0 + b1*(Tdie2 - Tref) + b2*pow((Tdie2 - Tref),2)
-#    fObj = (Vobj2 - Vos) + c2*pow((Vobj2 - Vos),2)
-#    tObj = pow(pow(Tdie2,4) + (fObj/S),.25)
-#    tObj = (tObj - 273.15)
-    
-#    print(round(tObj,2))
-#    return tObj
-    
+  
 def saveData(hexStr):
     #print "after expect"
     rval = hexStr.split()
@@ -69,11 +42,6 @@ def connect(tool):
     print "turn on temperature sensor"
     tool.sendline('char-write-cmd 0x29 01')
     tool.expect('\[LE\]>')
-
-#class SensorTag:
-#    def __init__(self,mac,control):
-#        self.mac = mac
-#        self.control = control
 
 def bleTempCollection(addresses, interval=1):
 
