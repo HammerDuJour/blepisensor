fa789043fc3e879cd1470f6747483875cbb6ce9f
jfgist <James.Gist@gmail.com>
2014-02-15 10:16:11 -0700

    first cut at UI with data

--- bletempUI.py
+++ bletempUI.py
@@ -1,9 +1,64 @@
-import time 
+
+import pexpect
+import sys
+import time
+import datetime
+
+from SensorTag import *
+from SensorCalcs import *
 from Tkinter import *
 from random import randint
 
+csvfile = r'testfile.csv'
+#set this empty to turn off logging:
+logfile = r'pexpect.log'
+
+
+def usage():
+    print 'blepisensor.py Usage:'
+    print '  blepisensor.py address [interval]'
+    print ''
+    print '  address    The address of the sensor to read.'
+    print '  interval   The reading interval in seconds.  Default is 1.'
+
+  
+def saveData(hexStr):
+    rval = hexStr.split()
+    objT = floatfromhex(rval[2] + rval[1])
+    ambT = floatfromhex(rval[4] + rval[3])
+    temp = calcTmpTarget(objT,ambT)
+    timestamp = datetime.datetime.now().strftime("%y-%m-%d-%H:%M:%S")
+    
+    f = open(csvfile,"a")
+    f.write("\"" + timestamp + "\",\"" + str(temp) + "\",\"" + str(ambT) + "\"\n")
+    f.close()
+    
+    return temp
+
+def connect(tool):
+    print "Connecting to Sensor Tag"
+    tool.sendline('connect')
+    tool.expect('Connection successful')   
+    tool.sendline('char-write-cmd 0x29 01')
+    tool.expect('\[LE\]>')
+
+def report_event(event):
+    event_name = {"2": "KeyPress", "4": "ButtonPress"}
+    print "Time:", str(event.time)   ### (6)
+    print "EventType=" + str(event.type), \
+        event_name[str(event.type)],\
+        "EventWidgetId=" + str(event.widget), \
+        "EventKeySymbol=" + str(event.keysym)      
+        
+def measure():
+    vals = {}
+    vals[0] = randint(45,99)
+    vals[1] = randint(45,99)
+    return vals
+
+# UI Code
 class MyApp:
-    def __init__(self,parent):
+    def __init__(self, parent, addresses, interval):
         
         self.myParent = parent
         self.myContainerConfig = Frame(parent, bg="black")
@@ -25,21 +80,50 @@ class MyApp:
         self.myContainerN.pack()
         
         self.createWidgets()
-        self.getMeasurements()
+        self.getMeasurements(addresses)
     
 
-    def getMeasurements(self):
-        while True:
-            vals = measure()
-            self.DataAmbient0["text"] = vals[0]
-            self.DataIR0["text"] = vals[1]
-
-            vals = measure()
-            self.DataAmbient1["text"] = vals[0]
-            self.DataIR1["text"] = vals[1]
+    def getMeasurements(self, addresses, interval):
+               
+        tools = []
+        SensorTags = []
+        
+        # create a collection of pexpect tools
+        for address in addresses:
+            if logfile != '':
+                print "Create tool with log file"
+                lf = open(logfile, 'a')
+                tool = pexpect.spawn('gatttool -b ' + address + ' --interactive',logfile=lf)
+            else:
+                print "Create tool, No log file"
+                tool = pexpect.spawn('gatttool -b ' + address + ' --interactive')        
             
-            time.sleep(1)
-            root.update()
+            tool.expect('\[LE\]>')
+            connect(tool)
+            tools.append(tool)
+            st = SensorTag(address,tool)
+            SensorTags.append(st)
+        
+        while True:
+            
+            for sensorTag in SensorTags:
+                tool = sensorTag.control
+                self.DataAddress0["text"] = sensorTag.mac
+                
+                tool.sendline('char-read-hnd 0x25')
+                time.sleep(float(interval))
+                index = tool.expect (['descriptor: .*', 'Disconnected', pexpect.EOF, pexpect.TIMEOUT],3)
+                
+                if index == 0:
+                    temp = saveData(tool.after)
+                elif index == 1:
+                    connect(tool)
+                    
+                self.DataIR0["text"] = str(temp)
+            
+                root.update()
+            
+        lf.close()
                     
        
     def createWidgets(self):
@@ -110,23 +194,10 @@ class MyApp:
         report_event(event)
         self.myParent.destroy()
         
-        
-def report_event(event):
-    event_name = {"2": "KeyPress", "4": "ButtonPress"}
-    print "Time:", str(event.time)   ### (6)
-    print "EventType=" + str(event.type), \
-        event_name[str(event.type)],\
-        "EventWidgetId=" + str(event.widget), \
-        "EventKeySymbol=" + str(event.keysym)      
-        
-def measure():
-    vals = {}
-    vals[0] = randint(45,99)
-    vals[1] = randint(45,99)
-    return vals
-        
+    
+addresses = ['BC:6A:29:AB:D5:92','BC:6A:29:AB:23:DA']
        
 root = Tk()
-myapp = MyApp(root)
+myapp = MyApp(root, addresses, 2)
 root.mainloop()
         
