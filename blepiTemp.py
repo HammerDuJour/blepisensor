b16c75fd6c84d19c5ace422bf7ebac69cc7a6370
jfgist <James.Gist@gmail.com>
2014-04-30 13:50:44 -0600

    Update blepiTemp.py

--- blepiTemp.py
+++ blepiTemp.py
@@ -10,23 +10,29 @@ csvfile = r'testfile.csv'
 logfile = r'pexpect.log'
 
 def usage():
-    print 'blepisensor.py Usage:'
-    print '  blepisensor.py address [interval]'
+    print 'blepiTemp.py Usage:'
+    print '  blepiTemp.py'
     print ''
-    print '  address    The address of the sensor to read.'
-    print '  interval   The reading interval in seconds.  Default is 1.'
-
+    print '  SensorTag addresses and labels are hardcoded into blepiTemp.py'
   
-def saveData(hexStr):
-    rval = hexStr.split()
-    objT = floatfromhex(rval[2] + rval[1])
-    ambT = floatfromhex(rval[4] + rval[3])
-    temp = calcTmpTarget(objT,ambT)
+def saveData(data):
     timestamp = datetime.datetime.now().strftime("%y-%m-%d-%H:%M:%S")
-    
     f = open(csvfile,"a")
-    f.write("\"" + timestamp + "\",\"" + str(temp) + "\",\"" + str(ambT) + "\"\n")
+    f.write("\"" + timestamp + "\"")
+    for dataPoint in data:
+        f.write(",\"" + str(dataPoint) + "\"")
+    f.write("\n")
     f.close()
+
+def saveDataToDB(temp,ambTemp,tagAddr,ipAddr):
+    connection = sqlite3.connect('/home/pi/blepimesh/data/client.db')
+    cursor = connection.cursor()
+    
+	cursor.execute("INSERT INTO log(tagDate,logDate,temp,ambTemp,tagAddr,ipAddr)VALUES(?,?,?,?,?,?)",
+	date('now'),time('now'), temp,ambTemp,tagAddr,ipAddr)
+    
+    connection.commit()
+    connection.close()
 
 def connect(tool):
     print "Connecting to Sensor Tag"
@@ -35,7 +41,7 @@ def connect(tool):
     tool.sendline('char-write-cmd 0x29 01')
     tool.expect('\[LE\]>')
 
-def bleTempCollection(addresses, interval=1):
+def bleTempCollection(addresses, descriptions, interval=1):
 
     print "Create Tools Variable"
     tools = []
@@ -54,35 +60,41 @@ def bleTempCollection(addresses, interval=1):
         tool.expect('\[LE\]>')
         connect(tool)
         tools.append(tool)
-        st = SensorTag(address,tool)
+        st = SensorTag(address,tool,descriptions[i])
+        i = i + 1
         SensorTags.append(st)
   
     #iterate over each tool in tools and retrieve temp data
     while True:
         for sensorTag in SensorTags:
+            
             tool = sensorTag.control
-            print(sensorTag.mac)
             tool.sendline('char-read-hnd 0x25')
-            time.sleep(float(interval))
             index = tool.expect (['descriptor: .*', 'Disconnected', pexpect.EOF, pexpect.TIMEOUT],3)
             if index == 0:
-                saveData(tool.after)
+                hexStr = tool.after
+                ambient = ambientTemp(hexStr)
+                ir = irTemp(hexStr)
             elif index == 1:
                 connect(tool)
+                
+            saveData(["ambientTemp", ambient, "IR Temp", irT])
+			saveDataToDB(irT,ambient,sensorTag.mac,0)
+			
+        time.sleep(float(interval))
                 
     # will this crash if lf is not created ?
     lf.close()        
     
 #TODO: cleanup usage and catch erroneous input in main
 #TODO: Create a DEbug() function and flag next time I need to debug
-#Wrap this thing in some UI code (maybe)
 
-    
 def main():
     if (len(sys.argv) < 2):
         # get mac addresses from file
-        macs = ['BC:6A:29:AB:D5:92','BC:6A:29:AB:23:DA']
-        bleTempCollection(macs)
+        addresses = ['BC:6A:29:AB:D5:92','BC:6A:29:AB:23:DA','BC:6A:29:AB:3B:4B', 'BC:6A:29:AB:23:F6']
+        descriptions = ['Tag 1','Tag 2','Tag 3','Tag 4']        
+        bleTempCollection(addresses,descriptions,sys.argv[1])
     elif (len(sys.argv) == 2):
         bleTemp(sys.argv[1])
     else:
