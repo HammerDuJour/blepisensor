f2e7a31338828902235a717030026264908cc24d
jfgist <James.Gist@gmail.com>
2014-04-30 16:11:38 -0600

    Update BlepiInit.py

--- BlepiInit.py
+++ BlepiInit.py
@@ -6,11 +6,10 @@ cursor = connection.cursor()
 
 print "Adding Data To DB"
 
-cursor.execute("CREATE TABLE SensorTags(Id INT, Address TEXT, Description TEXT)")
-cursor.execute("INSERT INTO SensorTags(1,'BC:6A:29:AB:D5:92','TAG 1')")
-cursor.execute("INSERT INTO SensorTags(1,'BC:6A:29:AB:23:DA','TAG 2')")
-cursor.execute("INSERT INTO SensorTags(1,'BC:6A:29:AB:3B:4B','TAG 3')")
-cursor.execute("INSERT INTO SensorTags(1,'BC:6A:29:AB:23:F6','TAG 4')")
+cursor.execute("INSERT INTO SensorTags values('1','BC:6A:29:AB:D5:92','TAG 1')")
+cursor.execute("INSERT INTO SensorTags values('2','BC:6A:29:AB:23:DA','TAG 2')")
+cursor.execute("INSERT INTO SensorTags values('3','BC:6A:29:AB:3B:4B','TAG 3')")
+cursor.execute("INSERT INTO SensorTags values('4','BC:6A:29:AB:23:F6','TAG 4')")
 
 connection.commit()
 
